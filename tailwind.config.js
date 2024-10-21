dc356985122cc7409a0c587b819221c0ed0ac865
Max <max0000maxmax@yandex.ru>
2024-10-21 06:23:13 +0500

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    screens: {
+      mob: {"max": "26.125rem"},
+    },
+    colors: {
+      black: "#000000",
+      white: "#ffffff",
+      blue: "#0070B7",
+      darkLiver:"#4E4F4D",
+      lightGray: "rgba(213, 213, 212, 0.7)", //#D5D5D4
+      graniteGray: "#626262",
+    },
     extend: {},
   },
   plugins: [],
