2482c8a33598105642f470786717a533a64ad99e
Max <max0000maxmax@yandex.ru>
2024-12-01 18:46:49 +0500

    .

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+function pain() {
   let pain = document.querySelectorAll(".pain");
   let brainPain = document.querySelector(".brain-pain");
 
@@ -12,4 +12,29 @@ document.addEventListener("DOMContentLoaded", function () {
       brainPain.classList.add("pain-opacity-0");
     });
   });
-});
+}
+
+function checkWidth() {
+  var width = window.innerWidth;
+  if (width > 450) {
+    pain();
+  }
+}
+
+window.onload = checkWidth;
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   let pain = document.querySelectorAll(".pain");
+//   let brainPain = document.querySelector(".brain-pain");
+
+//   pain.forEach(function (element) {
+//     element.addEventListener("mouseover", function () {
+//       brainPain.classList.remove("pain-opacity-0");
+//       brainPain.classList.add("pain-opacity");
+//     });
+//     element.addEventListener("mouseout", function () {
+//       brainPain.classList.remove("pain-opacity");
+//       brainPain.classList.add("pain-opacity-0");
+//     });
+//   });
+// });
