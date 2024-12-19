e0bb7ce43cdf8e94da5c119d921a268f0d6489fd
Max <max0000maxmax@yandex.ru>
2024-12-20 02:00:44 +0500

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
       lightGray: "rgba(213, 213, 212, 0.99)", //#D5D5D4
       graniteGray: "#626262",
       zincDust: "#5B5B5B",
-      accentText: "rgb(62, 62, 62)",
+      accentText: "rgb(60, 60, 60)",
     },
     extend: {},
   },
